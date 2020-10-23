c4e3a55cdb41ae20fff3e5f0cd938f555b922598
dogukan <dgknn.bas@gmail.com>
2020-10-23 18:29:06 +0300

    first commit

---
+++ vtys/IsLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vtys
+{
+    public static class IsLogin
+    {
+        public static bool Login = false;
+        public static string KAdi = "";
+    
+    }
+}
